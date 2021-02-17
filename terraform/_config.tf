7f1fff386fb3e3eb21783d0e04c0dc48cdaadacd
Pedro Junqueira <pedrocj@gmail.com>
2021-02-17 20:06:15 +1030

    Add terraform config file

---
+++ terraform/_config.tf
@@ -0,0 +1,11 @@
+# Configurations
+locals {
+  # Enter Name Prefix to use a unique resource name
+  name_prefix = "comp-vision"
+  # Enter your Azure Cognitive Services End Point
+  end_point = "END POINT"
+  # Enter your Azure Subscription Key
+  subscrition_key = "KEY"
+  # Enter the closest Azure location
+  location = "Australia East"
+}
